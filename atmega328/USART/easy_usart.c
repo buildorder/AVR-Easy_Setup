1b3973bd113b577b983bb951f3ea059e12c7085d
MinSeok Kim <buildorder@naver.com>
2017-02-12 23:05:44 +0900

    Fisrt Update - (Add Calculate UBBR)

---
+++ atmega328/USART/easy_usart.c
@@ -0,0 +1,8 @@
+﻿#include "easy_usart.h"
+
+uint32_t caculate_ubbr(uint32_t baudrate, uint32_t cpu_frequency)
+{
+	float ubbr = roundf( (float)cpu_frequency / ( 16 * baudrate) -1 ); 
+	
+	return (uint32_t)ubbr;
+}
---
+++ atmega328/USART/easy_usart.h
@@ -0,0 +1,23 @@
+﻿#ifndef EASY_USART_H_
+#define EASY_USART_H_
+
+#include <stdint.h>
+#include <math.h>
+
+//# BAUD RATE #
+#define BAUD_RATE_2400    2400
+#define BAUD_RATE_4800    4800
+#define BAUD_RATE_9600    9600
+#define BAUD_RATE_14400   14400
+#define BAUD_RATE_19200   19200
+#define BAUD_RATE_38400   38400
+#define BAUD_RATE_57600   57600
+#define BAUD_RATE_115200  115200
+//#BAUD RATE #
+
+#define RXEN        0x10
+#define TXEN        0x08
+
+uint32_t caculate_ubbr(uint32_t baudrate, uint32_t cpu_frequency);
+
+#endif /* EASY_USART_H_ */
