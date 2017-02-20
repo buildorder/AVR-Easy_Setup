3eab1a30c2a71eb038c77c5eb1fbf219b9657b9f
MinSeok Kim <buildorder@naver.com>
2017-02-20 13:33:50 +0900

    Add easy_setup / You can use printf, scanf

--- atmega328/USART/easy_usart.c
+++ atmega328/USART/easy_usart.c
@@ -1,5 +1,17 @@
 ﻿#include "easy_usart.h"
 
+void easy_uart_setup(uint32_t baudrate, uint32_t cpu_frequency)
+{
+	uint32_t ubbr = caculate_ubbr(baudrate, cpu_frequency);
+	set_ubbr(ubbr);
+	
+	set_send_character_size(DATA_8BIT);
+	
+	UCSR0B = (RXEN | TXEN);
+	
+	stdin = stdout = (FILE *)&full_stdio;	
+}
+
 uint32_t caculate_ubbr(uint32_t baudrate, uint32_t cpu_frequency)
 {
 	float ubbr = roundf( (float)cpu_frequency / ( 16 * baudrate) -1 ); 
@@ -15,5 +27,23 @@ void set_ubbr(uint32_t ubbr)
 
 void set_send_character_size(uint8_t character_size)
 {
-	UCSR0C = character_size << 1;
+	UCSR0C |= character_size << 1;
+}
+
+int file_uart_tx(char ch, FILE *stream)
+{
+	if (ch == '\n')
+		file_uart_tx('\r', stream);
+
+	while (!(UCSR0A & UDRE));
+	UDR0 = ch;
+	
+	return 0;
+}
+
+int file_uart_rx(FILE *stream)
+{
+	while (!(UCSR0A & RXC));
+	
+	return UDR0;
 }
--- atmega328/USART/easy_usart.h
+++ atmega328/USART/easy_usart.h
@@ -1,9 +1,22 @@
-﻿#ifndef EASY_USART_H_
+﻿/*
+ *		ORDER
+ *		
+ *		1. DEFINE
+ *		2. STRUCT
+ *		3. ENUM
+ *		4. FUNCTION
+ *		5. VARIABLE
+ */
+
+#ifndef EASY_USART_H_
 #define EASY_USART_H_
 
 #include <avr/io.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <math.h>
+
+#define UART_REG    0xC0
 
 //# BAUD RATE #
 #define BAUD_RATE_2400    2400
@@ -14,7 +27,11 @@
 #define BAUD_RATE_38400   38400
 #define BAUD_RATE_57600   57600
 #define BAUD_RATE_115200  115200
-//#BAUD RATE #
+//# BAUD RATE #
+
+#define RXC     0x80
+#define TXC     0x40
+#define UDRE    0x20
 
 #define RXEN        0x10
 #define TXEN        0x08
@@ -26,10 +43,16 @@ enum
 	DATA_7BIT,
 	DATA_8BIT
 };
+void easy_uart_setup(uint32_t baudrate, uint32_t cpu_frequency);
 
 uint32_t caculate_ubbr(uint32_t baudrate, uint32_t cpu_frequency);
-
 void set_ubbr(uint32_t ubbr);
+
 void set_send_character_size(uint8_t character_size);
+
+int file_uart_tx(char ch, FILE *stream);
+int file_uart_rx(FILE *stream);
+
+static const FILE full_stdio = FDEV_SETUP_STREAM(file_uart_tx, file_uart_rx, _FDEV_SETUP_RW);
 
 #endif /* EASY_USART_H_ */
