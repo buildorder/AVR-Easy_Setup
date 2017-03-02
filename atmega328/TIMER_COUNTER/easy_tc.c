264e22fda80422625d5620cdd69c48c5213dd2c4
MinSeok Kim <buildorder@naver.com>
2017-03-02 11:12:11 +0900

    Add Timer Counter c and h

---
+++ atmega328/TIMER_COUNTER/easy_tc.c
@@ -0,0 +1,8 @@
+﻿#include "easy_tc.h"
+
+void timer_init(uint8_t clock_frequency)
+{
+	TCCR0B |= (CLOCK_SET_BIT & clock_frequency);
+	
+	TIMSK0 |= TIMER0_OVF_INTERRUPT;
+}
---
+++ atmega328/TIMER_COUNTER/easy_tc.h
@@ -0,0 +1,33 @@
+﻿/*
+ * easy_tc.h
+ *
+ * Created: 2017-02-20 오후 5:02:41
+ *  Author: kms014
+ */ 
+
+
+#ifndef EASY_TC_H_
+#define EASY_TC_H_
+
+#include <avr/io.h>
+
+#define TIMER0_OVF_INTERRUPT 0x01
+
+#define CLOCK_SET_BIT 0x07
+
+enum clock_select
+{
+	NO_CLOCK,
+	CLOCK_1,
+	CLOCK_8,
+	CLOCK_64,
+	CLOCK_256,
+	CLOCK_1024,
+	FALLING_EXTERNAL_CLOCK,
+	RISING_EXTERNAL_CLOCK	
+};
+
+
+void timer_init(uint8_t clock_frequency);
+
+#endif /* EASY_TC_H_ */
